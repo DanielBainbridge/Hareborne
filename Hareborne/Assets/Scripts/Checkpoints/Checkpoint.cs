748782cd0318e7220c538a3ded42fa65b79b0115
Kai Van Der Staay <s214637@students.aie.edu.au>
2022-09-28 13:37:39 +1000

    added a few things
    added the function for getting the current time
    changed the m_recirdedTime to get the current time and added a foreach  for the checkpoints to make sure the player has passed through all for the level
    also added the auther thingy

--- Hareborne/Assets/Scripts/Checkpoints/Checkpoint.cs
+++ Hareborne/Assets/Scripts/Checkpoints/Checkpoint.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//author Kai
 public class Checkpoint : MonoBehaviour
 {
     float m_RecordedTime;
     [HideInInspector]
     public bool m_triggered = false;
     private CheckpointSystem m_parentSystem;
+    public Timer m_timer;
 
     /// <summary>
     /// Get the parent system for references to the player in the scene
@@ -27,15 +28,21 @@ public class Checkpoint : MonoBehaviour
         if (other.gameObject.GetComponent<PlayerController>())
         {
             m_parentSystem.m_player.SetRespawn(transform.position);
-            //change this line
-            m_RecordedTime = Time.time;
+            m_RecordedTime = m_timer.GetCurrentTime();
 
-            //
             int siblingIndex = transform.GetSiblingIndex();
             m_parentSystem.m_checkpoints[siblingIndex + 1].gameObject.SetActive(true);
             gameObject.SetActive(false);
 
             //check if all checkpoints in parent Checkpoint system are hit
+            foreach(Checkpoint c in m_parentSystem.m_checkpoints)
+            {
+                if(!c.m_triggered)
+                {
+                    break;
+                }
+                //do win stuffs
+            }
         }
     }
 }
